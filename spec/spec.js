a5c88494bc74d500ca5a51f508c3c2c7920330c8
simpletrain <zsy498761554@live.com>
2015-08-28 16:55:58 +0800

    step-2

---
+++ spec/spec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var Person = require('../src/main.js');
+
+describe('Person', function () {
+
+    it('should has name,age properties', function () {
+        var person = new Person('Tom', 21);
+        expect(person.name).toBe('Tom');
+        expect(person.age).toBe(21);
+    });
+
+    it('should print right introduce', function () {
+        var person = new Person('Tom', 21);
+        var introduce = person.introduce();
+        expect(introduce).toBe('My name is Tom. I am 21 years old.');
+    });
+
+});
